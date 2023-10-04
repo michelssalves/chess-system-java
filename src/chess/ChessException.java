eab29c6002adeaf1420bc1508edeb57044d43083
Michel Alves <82483090+michelssalves@users.noreply.github.com>
2023-10-04 09:45:19 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,14 @@
+package chess;
+
+public class ChessException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException (String msg) {
+		super(msg);
+	}
+
+}
